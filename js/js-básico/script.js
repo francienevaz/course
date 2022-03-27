bcf23cfe145c498302fe297690a3f151eb2ac823
francienevaz <fran.vfraga@gmail.com>
2022-03-27 10:27:30 -0300

    updated

--- js/js-básico/script.js
+++ js/js-básico/script.js
@@ -1,1 +1,4 @@
-console.log(' Hello World!');
+var nome = prompt("Welcome, How's it going? What's your name?");
+
+alert(`Nice to meet you ${nome}! I hope you enjoy it, Let's go to learning JavaScript! `);
+
