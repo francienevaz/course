42db2f05aafbcdc0f147212b225b25ad9aedd8ed
francienevaz <fran.vfraga@gmail.com>
2022-04-03 15:44:51 -0300

    updated conditions

--- js/js-básico/script.js
+++ js/js-básico/script.js
@@ -1,4 +1,13 @@
-var nome = prompt("Welcome, How's it going? What's your name?");
+// var nome = prompt("Welcome, How's it going? What's your name?");
 
-alert(`Nice to meet you ${nome}! I hope you enjoy it, Let's go to learning JavaScript! `);
+// alert(`Nice to meet you ${nome}! I hope you enjoy it, Let's go to learning JavaScript! `);
 
+function idade() {
+    var id = document.getElementById('idade');
+    var res = document.getElementById('result')
+    if (id.value >= 18) {
+        res.innerHTML=`Você tem ${id.value} anos! Está liberado para beber!`;
+    } else if (id.value < 18) {
+        res.innerHTML=`Você tem ${id.value} anos! Ainda não pode beber, volte quando tiver 18 anos!`
+    }
+}
