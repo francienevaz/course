46fb181cedb76ab11c7f36600c36975cbb463aa8
francienevaz <fran.vfraga@gmail.com>
2022-04-08 09:01:25 -0300

    updated

--- js/js-básico/Pages/switch.js
+++ js/js-básico/Pages/switch.js
@@ -1,23 +1,24 @@
-let nota1 = 10;
-let nota2 = 5;
-
-let media = (nota1 + nota2) / 2;
+let nota1 = document.getElementById('nota1');
+let nota2 = document.getElementById('nota2');
+let media = (nota1.value + nota2.value) / 2;
 let conceito = "";
 
-if (media < 7) {
-    conceito = "Insuficiente"
-} else if (media >= 7) {
-    conceito = "suficiente"
-}
+function consultar () {
+    if (media < 7) {
+        conceito = "Insuficiente"
+    } else if (media >= 7) {
+        conceito = "suficiente"
+    }
 
-switch (conceito) {
-    case "Insuficiente":
-        console.log(`Que pena! Sua média atual é ${media}, estude mais da próxima vez!`)
-        break;
-    case "suficiente":
-        console.log(`Parabéns! Sua média é ${media}, você passou!`) 
-        break;   
-    default:
-        console.log('Digite suas notas!')
-        break;
+    switch (conceito) {
+        case "Insuficiente":
+            console.log(`Que pena! Sua média atual é ${media}, estude mais da próxima vez!`)
+            break;
+        case "suficiente":
+            console.log(`Parabéns! Sua média é ${media}, você passou!`) 
+            break;   
+        default:
+            console.log('Digite suas notas!')
+            break;
+    } 
 }
