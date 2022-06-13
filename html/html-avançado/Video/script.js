5183f81dcfec12ffcf3dce78e681eef95260eee7
francienevaz <fran.vfraga@gmail.com>
2022-06-13 20:46:26 -0300

    updated

---
+++ html/html-avançado/Video/script.js
@@ -0,0 +1,25 @@
+    let video = document.getElementById("video1");
+
+    function  back() {
+
+    }
+
+    function  decrease() {
+
+    }
+
+    function play() {
+    video.play();
+    }
+
+    function  stopped() {
+
+    }
+
+    function increase() {
+
+    }
+
+    function  foward() {
+
+    }
