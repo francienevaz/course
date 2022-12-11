7c5572b8003078c1e9fa5500e08ca990104fb225
francienevaz <fran.vfraga@gmail.com>
2022-12-11 17:57:38 -0300

    updated

---
+++ Projects/Jogo-da-Memoria/assets/script.js
@@ -0,0 +1,17 @@
+const  FRONT = "card_front";
+const BACK = "card_back";
+
+let hogwarts = [
+    'dumbledore',
+    'harry',
+    'rony',
+    'hermione',
+    'voldemort',
+    'óculos',
+    'snape',
+    'chapéu seletor',
+    'dobby',
+    'patrono'
+];
+
+function createCardsFromHogwarts ()
