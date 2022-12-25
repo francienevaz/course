7f0da612a720b99fa4c7ebdc05fdafd3432a26fb
francienevaz <fran.vfraga@gmail.com>
2022-12-25 15:13:41 -0300

    updated jogo da memoria

--- Projects/Jogo-da-Memoria/assets/script.js
+++ Projects/Jogo-da-Memoria/assets/script.js
@@ -1,5 +1,7 @@
 const  FRONT = "card_front";
 const BACK = "card_back";
+const CARD = "card";
+const ICON = 'icon'
 
 let hogwarts = [
     'dumbledore',
@@ -14,13 +16,82 @@ let hogwarts = [
     'patrono'
 ];
 
-createCardsFromHogwarts(hogwarts)
+let cards = null;
+
+startGame();
+
+function startGame() {
+    cards = createCardsFromHogwarts(hogwarts);
+    shuffleCards(cards);
+    initializeCards(cards);
+}
+
+function initializeCards(cards) {
+    let gameBoard = document.getElementById("gameBoard");
+
+    cards.forEach( card => {
+        let cardElement = document.createElement('div');
+        cardElement.id = card.id;
+        cardElement.classList.add(CARD);
+        cardElement.dataset.icon = card.icon;
+
+        createCardContent(card, cardElement)
+
+        cardElement.addEventListener('click', flipCard);
+        gameBoard.appendChild(cardElement);
+
+    })
+
+}
+
+function createCardContent (card, cardElement) {
+        createCardFace(FRONT, card, cardElement);
+        createCardFace(BACK, card, cardElement);
+}
+
+function createCardFace (face, card, element) {
+    let cardElementFace = document.createElement('div');
+    cardElementFace.classList.add(face);
+
+    if(face === FRONT){
+
+        let iconElement = document.createElement('img');
+
+        iconElement.classList.add(ICON);
+        iconElement.src = "/Projects/Jogo-da-Memoria/imgs/" + card.icon + ".png" ;
+        cardElementFace.appendChild(iconElement)
+    } else {
+        cardElementFace.innerHTML = '&lt/&gt';
+    }
+
+    element.appendChild(cardElementFace);
+
+}
+
+function shuffleCards(cards) {
+    let currentIndex = cards.length;
+    let randomIndex = 0;
+
+    while(currentIndex !== 0) {
+
+        randomIndex = Math.floor(Math.random() * currentIndex)
+        currentIndex--;
+
+        [cards[randomIndex], cards[currentIndex]] = [cards[currentIndex], cards[randomIndex]]
+
+    }
+}
+
+
 function createCardsFromHogwarts (characters) {
     let cards = [];
 
-    for(let character of characters){
-        cards.push(createPairFromHogwarts(character))
-    }
+    characters.forEach((character) => {
+        cards.push(createPairFromHogwarts(character));
+    })
+    // for(let character of characters){
+    //     cards.push(createPairFromHogwarts(character))
+    // }
 
     return cards.flatMap(pair => pair);
 }
@@ -40,4 +111,9 @@ function createPairFromHogwarts(character){
 
 function createIdWithCharacter(character) {
     return character + parseInt(Math.random() * 1000);
+}
+
+function flipCard() {
+
+    this.classList.add("flip");
 }
