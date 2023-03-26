615c6e3994b4d70c93fe8e97bdd7e00d684863b3
francienevaz <fran.vfraga@gmail.com>
2023-03-26 16:32:45 -0300

    updated

--- Node/NodeJS/moduloHttp/index.js
+++ Node/NodeJS/moduloHttp/index.js
@@ -2,9 +2,9 @@ const http = require('http');
 
 http.createServer((resquest, response) => {
     response.writeHead(200, {
-        'Content-Type': 'text/plain'
+        'Content-Type': 'text/html'
     });
-    response.end("Hello World!");
+    response.end('<h1>Hello World!</h1>');
 }).listen(3000, (err) => {
     if (err) {
         console.log(err)
