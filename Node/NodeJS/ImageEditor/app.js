a7e32e9d669614b164aa0880e26555d7db0441f3
francienevaz <fran.vfraga@gmail.com>
2023-03-26 16:11:47 -0300

    updated

--- Node/NodeJS/ImageEditor/app.js
+++ Node/NodeJS/ImageEditor/app.js
@@ -1,16 +1,42 @@
 const sharp = require("sharp");
+const compress_images = require("compress-images");
+const fs = require('fs');
 
 let path = process.argv[2];
 let width = Number(process.argv[3]);
+let output = "./temp/output_resizeIII.jpg";
 
-function resize (path, width) {
-    sharp(path).resize({width: width}).toFile("./temp/output_resizeII.jpg", (err) => {
+function resize (inputPath, outputPath, width) {
+    sharp(inputPath).resize({width: width}).toFile(outputPath, (err) => {
         if(err){
             console.log(err);
         } else {
             console.log("Imagem redimensionada com sucesso")
+            compress(outputPath, "./compressed/");
         }
     })
 }
+function compress (pathInput, outputPath){
+    compress_images(pathInput, outputPath, { compress_force: false, statistic: true, autoupdate: true }, false,
+        { jpg: { engine: "mozjpeg", command: ["-quality", "60"] } },
+        { png: { engine: "pngquant", command: ["--quality=20-50", "-o"] } },
+        { svg: { engine: "svgo", command: "--multipass" } },
+        { gif: { engine: "gifsicle", command: ["--colors", "64", "--use-col=web"] } },
+        function (error, completed, statistic) {
+            console.log("-------------");
+            console.log(error);
+            console.log(completed);
+            console.log(statistic);
+            console.log("-------------");
 
-resize(path, width);
+            fs.unlink(pathInput, (err) => {
+                if(err) {
+                    console.log(err)
+                } else {
+                    console.log(pathInput, " Apagado")
+                }
+            })
+        }
+    );
+}
+resize(path, output, width);
