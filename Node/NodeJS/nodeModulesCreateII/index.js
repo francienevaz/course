2c72d0b4f2554842fce10a3672e32ae746d0302b
francienevaz <fran.vfraga@gmail.com>
2023-03-26 11:54:46 -0300

    updated

--- Node/NodeJS/nodeModulesCreateII/index.js
+++ Node/NodeJS/nodeModulesCreateII/index.js
@@ -1,3 +1,6 @@
 let calc = require('matematica');
 
 console.log(calc.area_triangulo(150, 46));
+
+
+// Caso a pasta onde está as funções, ou seja, o pacote node que criamos seja apagada, basta usar o comando npm install, que o mesmo irá instalar o pacote novamente, sem a necessidade de colocar o endereço da pasta, pois no arquivo json já está instalado como dependências do projeto
