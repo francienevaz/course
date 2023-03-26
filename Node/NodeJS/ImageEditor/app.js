b9d5412e385dbb556a0bc97a66a785b3d71c025c
francienevaz <fran.vfraga@gmail.com>
2023-03-26 14:28:29 -0300

    add ImageEditor's paste

---
+++ Node/NodeJS/ImageEditor/app.js
@@ -0,0 +1,16 @@
+const sharp = require("sharp");
+
+let path = process.argv[2];
+let width = Number(process.argv[3]);
+
+function resize (path, width) {
+    sharp(path).resize({width: width}).toFile("./temp/output_resizeII.jpg", (err) => {
+        if(err){
+            console.log(err);
+        } else {
+            console.log("Imagem redimensionada com sucesso")
+        }
+    })
+}
+
+resize(path, width);
