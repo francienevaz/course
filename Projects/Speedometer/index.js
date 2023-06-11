baaa377006c94db0486fd324b774512061e441fa
francienevaz <fran.vfraga@gmail.com>
2023-06-11 12:16:38 -0300

    updated speedometer

--- Projects/Speedometer/index.js
+++ Projects/Speedometer/index.js
@@ -4,14 +4,25 @@ const allRides = getAllRides()
 allRides.forEach(async ([id, value]) => {
 	const ride = JSON.parse(value)
 	ride.id = id
-	
-	console.log(ride)
 
 	const firstPosition = ride.data[0]
 	const firstLocationData = await getLocationData(firstPosition.latitude, firstPosition.longitude);
 	const itemElement = document.createElement("li")
 	itemElement.id = ride.id
 	itemElement.innerText = `${firstLocationData.city}-${firstLocationData.countryCode}`
+
+	const cityDiv = document.createElement("div");
+	cityDiv.innerText = `${firstLocationData.city}`
+
+	const maxSpeedDiv = document.createElement("div");
+	maxSpeedDiv.innerText = `Max Speed: ${getMaxSpeed(ride.data)} Km/h`
+
+	const distanceDiv = document.createElement("div")
+	distanceDiv.innerText = `Distance: ${getDistance(ride.data)} Km`
+
+	itemElement.appendChild(cityDiv)	
+	itemElement.appendChild(maxSpeedDiv)
+	itemElement.appendChild(distanceDiv)
 	rideListElement.appendChild(itemElement)
 })
 
@@ -21,4 +32,57 @@ async function getLocationData (latitude, longitude) {
 	const response = await fetch(url)
 	
 	return await response.json()
+}
+
+function getMaxSpeed (positions) {
+	let maxSpeed = 0
+	 
+	positions.forEach(position => {
+		if(position.speed != null && position.speed > maxSpeed){
+			maxSpeed = position.speed
+		}
+	})
+
+	// 1 m/s
+	// 60 m/min
+	// 3600 m/h
+	// 3.6 km/h
+
+	return (maxSpeed * 3.6).toFixed(1)
+}
+
+function getDistance (positions) {
+	const earthRadiusKm = 6371
+	let totalDistance = 0
+
+	for(let i = 0; i < positions.length - 1; i++) {
+		const p1 = {
+			latitude: positions[i].latitude,
+			longitude: positions[i].longitude
+		}
+		const p2 = {
+			latitude: positions[i + 1].latitude,
+			longitude: positions[i + 1].longitude
+		}
+
+		const deltaLatitude =toRad(p2.latitude - p1.latitude)
+
+		const deltaLongitude =toRad(p2.longitude - p1.longitude)
+
+		const a = Math.sin(deltaLatitude / 2) * Math.sin(deltaLatitude / 2) +
+		Math.sin(deltaLongitude / 2) * Math.sin(deltaLongitude / 2) *
+		Math.cos(toRad(p1.latitude)) * 
+		Math.cos(toRad(p2.latitude)) 
+
+		const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a))
+
+		const distance = earthRadiusKm * c
+		totalDistance += distance
+	}
+
+	function toRad(degree) {
+		return degree * Math.PI / 180
+	}
+
+	return totalDistance.toFixed(2)
 }
