3537dd5dad6727004cc353ed4edfc1b3b3942080
francienevaz <fran.vfraga@gmail.com>
2023-06-28 19:25:38 -0300

    updated middleware file

--- Node/NodeExpress/Install/middleware.js
+++ Node/NodeExpress/Install/middleware.js
@@ -1,5 +1,7 @@
 let express = require('express');
 let app = express();
+let bodyParse = require("body-parser");
+const bodyParser = require('body-parser');
 
 
 //Uma das características do Middleware é que ele tem como parâmetro a requisição, a resposta e o next... o next quando passado um parâmetro retorna uma mensagem de erro, definida pelo programador
@@ -12,8 +14,8 @@ let consoleBody = (req, res, next) => {
 let hello = (req, res) => {
     res.send("hello world");
 }
-
-app.use("/", consoleMethod) // o use responde a qualquer tipo de método (get, post, delete, update)
+app.use("/", bodyParser.json()) //através do bodyParser conseguimos ver no console o conteúdo que estiver no Body
+app.use("/", consoleBody) // o use responde a qualquer tipo de método (get, post, delete, update)
 app.get("/", hello)
 app.post("/", hello)
 
