a3c7659ed8dac32557ee6821436573274e0fed7d
francienevaz <fran.vfraga@gmail.com>
2023-07-08 19:14:14 -0300

    updated

--- Projects/Mural-de-Avisos/index.js
+++ Projects/Mural-de-Avisos/index.js
@@ -17,10 +17,20 @@ app.get("/all", (req, res) => {
 
 })
 
-app.post("/new", (req, res) => {
+app.post("/new", express.json(), (req, res) => {
+    let id = generateID();
+    let title = req.body.title;
+    let description = req.body.description;
 
+    posts.push({id, title, description});
+
+    res.send("Post adicinado");
 })
 
 app.listen(PORT, () => {
     console.log("Server running on port", PORT)
 })
+
+function generateID() {
+    return Math.random().toString(36).substring(2,9)
+}
