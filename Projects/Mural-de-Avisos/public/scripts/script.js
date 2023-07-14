fe3adda510072b6e970a085204d6a85b5473d74f
francienevaz <fran.vfraga@gmail.com>
2023-07-14 11:26:17 -0300

    updated

--- Projects/Mural-de-Avisos/public/scripts/script.js
+++ Projects/Mural-de-Avisos/public/scripts/script.js
@@ -1,9 +1,10 @@
+// "DomContentLoaded" para verificar se o conteúdo da pág está carregado e só assim chama a função updatedPosts()
 document.addEventListener("DOMContentLoaded", () => {
     updatePosts();
 })
 
 function updatePosts() {
-
+    // o método fetch pega os dados do backend, "api" para conseguirmos passar os dados para o frontend
     fetch("http://localhost:3000/api/all").then(res => {
         return res.json()
     }).then( json => {
@@ -13,8 +14,19 @@ function updatePosts() {
         // converte a resposta que a api manda em JSON para Objeto ☝🏻☝🏻
 
         posts.forEach((post) => {
-            let postElement = ``
+            let postElement = `
+            <div id=${post.id} class="card mb-4">
+                <div class="card-header">
+                    <h5 class="card-title">${post.title}</h5>
+                    <div class="card-body">
+                        <div class="card-text">${post.description}</div>
+                    </div>
+                </div>
+            </div>`
+            postElements += postElement
         });
+
+        document.getElementById("posts").innerHTML = postElements;
     })
 }
 
