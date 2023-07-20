9738d1ae9031b0bfbae811cf52618f8ee4d590b7
francienevaz <fran.vfraga@gmail.com>
2023-07-20 18:54:56 -0300

    updated

--- Node_Template_Engine/templatesII/index.js
+++ Node_Template_Engine/templatesII/index.js
@@ -5,18 +5,21 @@ const fs = require('fs');
 const path = require('path');
 const app = express();
 
+let user = {
+    name: "Johnny Deep", 
+    phone: "(333) 21548758"
+}
 
-
-app.set("views", path.join(__dirname, 'views')); // "views é a chave que estamos setando para que o ejs encontre o caminho da página que queremos renderizar "
-app.set("view engine", "ejs");
+app.set('views', path.join(__dirname, 'views')); // "views é a chave que estamos setando para que o ejs encontre o caminho da página que queremos renderizar "
+app.set('view engine', 'ejs');
 
 app.get("/", (req, res) => {
 
-    res.render("user");
+    res.render('user', user);
 
-})
+});
 
 
 app.listen(3000, () => {
     console.log("Server Running on 3000")
-})
+});
