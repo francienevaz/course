6984ccb610b070abfe11d109c67b79b35496a921
francienevaz <fran.vfraga@gmail.com>
2023-12-21 12:45:30 -0300

    updated

---
+++ Mongodb/Mongoose/index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://localhost:27017/links', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => {
+    console.log('Conexão bem-sucedida ao MongoDB');
+    // Faça operações após a conexão ser estabelecida
+  })
+  .catch((error) => {
+    console.error('Erro ao conectar ao MongoDB:', error);
+  });
+
+
+app.get('/', (req, res) => res.send('Hello World!'))
+app.listen(port, ()=> console.log('Example app listening on port 3000'))
