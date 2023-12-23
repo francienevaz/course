15011b019c6e2accaf03c4cba1e5db39e2258997
francienevaz <fran.vfraga@gmail.com>
2023-12-23 00:36:28 -0300

    update

--- Mongodb/Mongoose/index.js
+++ Mongodb/Mongoose/index.js
@@ -3,17 +3,23 @@ const app = express()
 const port = 3000
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost:27017/links', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-  .then(() => {
-    console.log('Conexão bem-sucedida ao MongoDB');
-    // Faça operações após a conexão ser estabelecida
-  })
-  .catch((error) => {
+  mongoose.connect('mongodb://localhost:27017')
+  .then(() => console.log("Conectado a MongoDB"))
+  .catch((error)=>{
     console.error('Erro ao conectar ao MongoDB:', error);
-  });
+  })
+
+//{
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// })
+//   .then(() => {
+//     console.log('Conexão bem-sucedida ao MongoDB');
+//     // Faça operações após a conexão ser estabelecida
+//   })
+//   .catch((error) => {
+//     console.error('Erro ao conectar ao MongoDB:', error);
+//   });
 
 
 app.get('/', (req, res) => res.send('Hello World!'))
