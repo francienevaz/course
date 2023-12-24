c416f7e7c432fb72a65680747c978c49c8d2596a
francienevaz <fran.vfraga@gmail.com>
2023-12-24 11:22:29 -0300

    updated

--- Mongodb/Mongoose/index.js
+++ Mongodb/Mongoose/index.js
@@ -3,36 +3,52 @@ const app = express()
 const port = 3000
 const mongoose = require('mongoose')
 
-const linkSchema = new mongoose.Schema({
-  title: String,
-  description: String, 
-  url: String,
-  click: Number
+// const linkSchema = new mongoose.Schema({
+//   title: String,
+//   description: String, 
+//   url: String,
+//   click: Number
+// })
+
+// const Link = mongoose.model('Link', linkSchema)
+
+// let link = new Link({
+//   title: "Fran",
+//   description: 'This is a test for the database',
+//   url: 'http://www.google.com',
+//   click: 0
+// })
+
+// link.save().then(doc => {
+//   console.log(doc)
+// }).catch(error => { 
+//   console.log(error)
+// })
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  age: Number,
 })
 
-const Link = mongoose.model('Link', linkSchema)
+const Person = mongoose.model('Person', personSchema);
 
-let link = new Link({
-  title: "Fran",
-  description: 'This is a test for the database',
-  url: 'http://www.google.com',
-  click: 0
-})
+let person = new Person({name: 'Pedro', age: 23});
 
-link.save().then(doc => {
-  console.log(doc)
-}).catch(error => { 
+person.save().then(doc => {
+  console.log("Document inserted", doc)
+}).catch(error => {
   console.log(error)
 })
 
-  mongoose.connect('mongodb://127.0.0.1:27017/newLinks')
 
-  let db = mongoose.connection
+mongoose.connect('mongodb://127.0.0.1:27017/newLinks')
 
-  db.on("error", console.error.bind(console, "MongoDB connection error:"))
-  db.once("open", ()=> {
-    console.log("Connected to MongoDB")
-  })
+let db = mongoose.connection
+
+db.on("error", console.error.bind(console, "MongoDB connection error:"))
+db.once("open", ()=> {
+  console.log("Connected to MongoDB")
+})
 
 
 app.get('/', (req, res) => res.send('Hello World!'))
