bdf670118320ec5c46c1b06e468f92c62dbd4fcb
francienevaz <fran.vfraga@gmail.com>
2023-12-24 09:16:35 -0300

    updated

--- Mongodb/Mongoose/index.js
+++ Mongodb/Mongoose/index.js
@@ -3,22 +3,14 @@ const app = express()
 const port = 3000
 const mongoose = require('mongoose')
 
-  mongoose.connect('mongodb://localhost:27017')
-  .then(() => console.log("Conectado a MongoDB"))
+// erro estava ocorrendo por causa do endereço, localhost não foi aceito, e faltava criar uma pasta data na pasta raiz
+
+  mongoose.connect('mongodb://127.0.0.1:27017/blog')
+  .then((db) => {
+    console.log("Conectado a MongoDB")
+    console.log(db)
+  })
   .catch((error)=> console.error('Erro ao conectar ao MongoDB:', error))
 
-//{
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// })
-//   .then(() => {
-//     console.log('Conexão bem-sucedida ao MongoDB');
-//     // Faça operações após a conexão ser estabelecida
-//   })
-//   .catch((error) => {
-//     console.error('Erro ao conectar ao MongoDB:', error);
-//   });
-
-
 app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, ()=> console.log('Example app listening on port 3000'))
+app.listen(port, () => console.log('Example app listening on port 3000'))
