4b110afb9d45456b4bb8b8171f3945165740896d
francienevaz <fran.vfraga@gmail.com>
2023-12-25 18:07:02 -0300

    updated

--- React/01-CreateReactApp/react-app/src/Header.js
+++ React/01-CreateReactApp/react-app/src/Header.js
@@ -5,13 +5,19 @@ import './App.css';
 // nesse exemploa abaixo o Header é um componente
 // as props - propriedades servem para facilitar a reutilização de componentes, trocando somente o valor da propriedade.
 
-function Header(props) {
-    return (
-        <header className="header">
-        <h1>{props.name}</h1>
-        <Menu links={props.links}></Menu>
-        </header>
-    )
+class Header extends React.Component{
+    constructor(props){
+        super(props)
+    }
+    
+    render(){
+        return (
+            <header className="header">
+            <h1>{this.props.name}</h1>
+            <Menu links={this.props.links}></Menu>
+            </header>
+        )
+    }
 }
 
 export default Header;
