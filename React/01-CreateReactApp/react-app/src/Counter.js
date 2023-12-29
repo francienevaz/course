adbaec1cd41903b8557321e9e0acc57a51f26f33
francienevaz <fran.vfraga@gmail.com>
2023-12-29 20:12:46 -0300

    updated

--- React/01-CreateReactApp/react-app/src/Counter.js
+++ React/01-CreateReactApp/react-app/src/Counter.js
@@ -1,47 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 
-class Counter extends React.Component{
+// useState é um hook, assim como useEffect
+function Counter(props) {
+    const [count, setCount] = useState(props.count);
 
-    constructor(props){
-        super(props);
-
-        this.state = {count: 0}
-        this.add = this.add.bind(this)
+    function add(){
+        setCount(count + 1);
     }
 
-    add(){
-        // this.setState({count: this.state.count + 1})
-        this.setState((state)=>{ return {count: state.count + 1}}, ()=>{
-            console.log(this.state)
-            //setando o state no localStorage
-            localStorage.setItem("state", JSON.stringify(this.state))
-        })
-    }
-
-    // componentdDidMount sera executado depois que o componente for renderizado na tela
-
-    componentDidMount(){
-        // renderizando o valor salvo no localStorage
-        this.setState(JSON.parse(localStorage.getItem("state")))
-    }
-
-    // Toda vez que mudamos o state o componente sera atualizado, a nao ser que chamemos a funcao shouldComponentUpdate() retornando o valor "false"
-    // shouldComponentUpdate(){
-    //     return false;
-    // }
-
-    //Esse metodo sera chamado sempre que o componente for removido dsa tela
-    componentWillUnmount(){
-        
-    }
-    render(){
-        return(
-            <div>
-                <h1>Counter: {this.state.count}</h1>
-                <button onClick={this.add}>Add</button>
-            </div>
-        )
-    }
+    return(
+        <div>
+            <h1>Counter: {count}</h1>
+            <button onClick={add}>Add</button>
+        </div>
+    )
 }
+
+// class Counter extends React.Component{
+
+//     constructor(props){
+//         super(props);
+
+//         this.state = {count: 0}
+//         this.add = this.add.bind(this)
+//     }
+
+//     add(){
+//         // this.setState({count: this.state.count + 1})
+//         this.setState((state)=>{ return {count: state.count + 1}}, ()=>{
+//             console.log(this.state)
+//             //setando o state no localStorage
+//             localStorage.setItem("state", JSON.stringify(this.state))
+//         })
+//     }
+
+//     // componentdDidMount sera executado depois que o componente for renderizado na tela
+
+//     componentDidMount(){
+//         // renderizando o valor salvo no localStorage
+//         this.setState(JSON.parse(localStorage.getItem("state")))
+//     }
+
+//     // Toda vez que mudamos o state o componente sera atualizado, a nao ser que chamemos a funcao shouldComponentUpdate() retornando o valor "false"
+//     // shouldComponentUpdate(){
+//     //     return false;
+//     // }
+
+//     //Esse metodo sera chamado sempre que o componente for removido dsa tela
+//     componentWillUnmount(){
+        
+//     }
+//     render(){
+//         return(
+//             <div>
+//                 <h1>Counter: {this.state.count}</h1>
+//                 <button onClick={this.add}>Add</button>
+//             </div>
+//         )
+//     }
+// }
 
 export default Counter;
