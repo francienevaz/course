cd5f218db0b8598a9c50b8a6ef45e7e5a62a1728
francienevaz <fran.vfraga@gmail.com>
2023-12-29 21:01:49 -0300

    updated

--- React/01-CreateReactApp/react-app/src/Counter.js
+++ React/01-CreateReactApp/react-app/src/Counter.js
@@ -1,8 +1,25 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 // useState é um hook, assim como useEffect
 function Counter(props) {
     const [count, setCount] = useState(props.count);
+
+    // Irá executar apenas uma vez, por causa do [], no fim
+    // o useEffect vai ser chamado quando o componente for criado, o equivalente ao didMount, e toda a vez que componente for atualizado o equivalente ao didUpdate
+
+    useEffect(()=>{
+        setCount(parseInt(localStorage.getItem("count")))
+    }, [])
+
+    // se nenhuma variável for setada para que o useEffect fique monitorando, qualquer estado quando atualizado irá executar a função, se setar a variável que deve ser monitorada, somente ela irá executar a função. 
+    // useEffect(()=>{
+    //     document.title = count
+    //     localStorage.setItem("count", count)
+    // }, [count]) => count está sendo observada
+    useEffect(()=>{
+        document.title = count;
+        localStorage.setItem("count", count);
+    }, [count])
 
     function add(){
         setCount(count + 1);
@@ -38,9 +55,15 @@ function Counter(props) {
 
 //     componentDidMount(){
 //         // renderizando o valor salvo no localStorage
+//         document.title = this.state.count;
 //         this.setState(JSON.parse(localStorage.getItem("state")))
 //     }
 
+//     componentDidUpdate(){
+//         // salvando o novo estado no localStorage a cada atualização do componente
+//         document.title = this.state.count;
+//         localStorage.setItem("state", JSON.stringify(this.state))
+//     }
 //     // Toda vez que mudamos o state o componente sera atualizado, a nao ser que chamemos a funcao shouldComponentUpdate() retornando o valor "false"
 //     // shouldComponentUpdate(){
 //     //     return false;
