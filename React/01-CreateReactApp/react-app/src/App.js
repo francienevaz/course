63495ec2a89957920e3a0541c14caea6d8878681
francienevaz <fran.vfraga@gmail.com>
2023-12-31 15:52:50 -0300

    updated

--- React/01-CreateReactApp/react-app/src/App.js
+++ React/01-CreateReactApp/react-app/src/App.js
@@ -1,14 +1,42 @@
 import './App.css';
 import Header from './Header';
 import Counter from './Counter'
+import { useEffect, useState } from 'react';
 
 function App() {
-  return (
+  const [show, setShow] = useState(true)
+
+  let time = 0;
+
+  useEffect(()=>{
+    let timer = setInterval(()=>{
+      time++;
+      console.log(time)
+  
+      if(time >= 5){
+        clearInterval(timer); 
+        setShow(false)
+      }
+    }, 1000)
+  }, [])
+
+
+  if (show){
+    return (
     <div>
       <Header name="Fran" links={["Sobre", "Comprar", "Contatos", "Login" ]}></Header>,
       <Counter count={0}></Counter>
     </div>
   );
+  } else {
+    return (
+      <div>
+        <Header name="Fran" links={["Sobre", "Comprar", "Contatos", "Login" ]}></Header>,
+        <h4>Sem contador</h4>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
--- React/01-CreateReactApp/react-app/src/Counter.js
+++ React/01-CreateReactApp/react-app/src/Counter.js
@@ -7,6 +7,17 @@ function Counter(props) {
     });
 
     useEffect(()=>{
+        setCount(parseInt(localStorage.getItem("count")));
+
+        //O que estiver dentro da função de retorno será executado quando o componente for desmontado, servindo como o equivalente ao componentWillUnmount para realizar limpeza de recursos ou interromper certas operações antes que o componente seja removido do DOM.
+        
+        return()=>{
+            console.log("Não tem mais contador")
+        }
+    }, [])
+
+
+    useEffect(()=>{
         document.title = count;
         localStorage.setItem("count", count.toString());
     }, [count])
