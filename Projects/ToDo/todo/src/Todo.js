93c8df2d87d229af29ab0e16a91ce2946c1d6c30
francienevaz <fran.vfraga@gmail.com>
2023-12-31 22:23:57 -0300

    updated

---
+++ Projects/ToDo/todo/src/Todo.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './Todo.css'
+
+
+function Todo(){
+
+    return(
+        <div className="container">
+            <h1>Hello World</h1>
+        </div>
+    )
+}
+
+export default Todo
