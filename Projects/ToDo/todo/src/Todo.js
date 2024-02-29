4e60f94b91ad5f8498d246c8f0bb10f9ed77112e
francienevaz <fran.vfraga@gmail.com>
2024-02-29 20:05:00 -0300

    updated

--- Projects/ToDo/todo/src/Todo.js
+++ Projects/ToDo/todo/src/Todo.js
@@ -5,20 +5,29 @@ import './Todo.css'
 function Todo(){
 
     const [text, setText] = useState('');
+    const  [items, setItems] = useState([]);
 
     function handleChange(event) {
         let t = event.target.value;
         setText(t);
     }
 
+    function addItem(event){
+        event.preventDefault();
+        if(text) {
+            setItems([...items, text]);
+            setText("");
+        }
+    }
+
     return(
         <div className="container">
             <h1>Hello World</h1>
             <form>
-                <input onChange={handleChange} type="text"></input>
-                <button type="button">Add Task</button>
+                <input onChange={handleChange} type="text" value={text}></input>
+                <button onClick={addItem} type="button">Add Task</button>
                 <ul>
-                    <li>{text}</li>
+                    {items.map((item, index) => <li key={index}>{item}</li>)}
                 </ul>
             </form>
         </div>
